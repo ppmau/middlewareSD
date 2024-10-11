6d3393b8bf5bd3412ddd63c8cab318aec989d75a
Mauricio Perez <80078495+ppmau@users.noreply.github.com>
2024-10-11 12:45:18 -0600

    Entregable 1
    Programa que envía mensajes entre nodos

---
+++ Entregable1/nodo.py
@@ -0,0 +1,120 @@
+import socket
+import threading
+import os
+from datetime import datetime
+PORT = 12345
+
+
+def server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        server_socket.bind(('localhost', PORT))
+        server_socket.listen(5)
+        while True:
+            client_socket, client_address = server_socket.accept()
+            data = client_socket.recv(1024).decode()
+            client_socket.send(f"El servidor {client_address[0]} ha recibido el mensaje: {data}".encode())
+            escribir_mensaje("nodeDB.txt","INSTRUCTION",data,"RECIVED",client_address[0],datetime.now())
+    except Exception as e:
+        print(f"Error en el servidor: {e}")
+    finally:
+        server_socket.close()
+
+def escribir_mensaje(archivo,instruccion,dato,estatus,nodo,fecha):
+    with open("nodeDB.txt", "r") as db:
+        contenido = db.read()
+        with open("nodeDB.txt", "a") as archivo:
+            if not contenido:
+                    archivo.write(f"{instruccion}|{dato}|{estatus}|{nodo}|{fecha}")
+            else:
+                archivo.write(f"\n{instruccion}|{dato}|{estatus}|{nodo}|{fecha}")
+
+
+def cliente(mensaje,puerto):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('localhost', puerto))
+    client_socket.send(mensaje.encode())
+    response = client_socket.recv(1024).decode()
+    escribir_mensaje("nodeDB.txt","INSTRUCTION",mensaje,"SEND",client_socket.getsockname()[0],datetime.now())
+    print(f"{response}")
+    client_socket.close()
+    input("\nEnter para continuar... ")
+
+
+def mostrar_opciones():
+    opcionAccion = 0
+    while opcionAccion != 3:
+            os.system('cls')
+            print("\n           Menú\n")
+            print("1. Enviar mensaje a un nodo\n")
+            print("2. Mostrar mensajes del nodo\n")
+            try:
+                opcionAccion = int(input("Selecciona una opción: "))
+                if opcionAccion == 1:
+                    print("Enviar mensaje a un nodo \n" )
+                    mostrar_menu_envio()
+                elif opcionAccion == 2:
+                    mostrar_mensajes()
+                elif opcionAccion == 3:
+                    print("Salir")
+                else:
+                    print("Opción no válida, por favor intenta de nuevo.")
+            except ValueError:
+                print("Por favor, ingresa un número válido.")
+
+def mostrar_mensajes():
+        datos = []
+        print("\n                Mensajes en el nodo     \n")
+        with open("nodeDB.txt", "r") as archivo:
+            for linea in archivo:
+                elementos = linea.strip().split('|')
+                datos.append(elementos)
+                print(linea)
+        input("\nEnter para continuar...")
+
+def mostrar_menu_envio():
+    opcionEnvio = 0
+    while opcionEnvio != 5:
+            try:
+                os.system('cls')
+                print("     Envio de mensajes\n")
+                print("1. Enviar mensaje al nodo 1")
+                print("2. Enviar mensaje al nodo 2")
+                print("3. Enviar mensaje al nodo 3")
+                print("4. Enviar mensaje al nodo 4")
+                print("5. Salir")
+                opcionEnvio = int(input("\nSelecciona una opción: "))
+                if opcionEnvio != 5:
+                    mensaje = input("Mensaje a enviar: ")
+                
+                if opcionEnvio == 1:
+                    print("Enviando mensaje a nodo 1... " )
+                    cliente(mensaje, 12345)
+                elif opcionEnvio == 2:
+                    print("Enviando mensaje a nodo 2... " )
+                    cliente(mensaje, 12346)
+                elif opcionEnvio == 3:
+                    print("Enviando mensaje a nodo 3... " )
+                    cliente(mensaje, 12347)
+                elif opcionEnvio == 4:
+                    print("Enviando mensaje a nodo 4... " )
+                    cliente(mensaje, 12348)
+                elif opcionEnvio == 5:
+                    print("Salir " )
+                else:
+                    input("Opción no válida, por favor intenta de nuevo. Enter para continuar...")
+            except ValueError:
+                input("Opción no válida, por favor intenta de nuevo. Enter para continuar...")
+
+
+
+if __name__ == "__main__":
+    # Crear el hilo para el servidor
+    server_thread = threading.Thread(target=server)
+    server_thread.start()
+    mostrar_opciones()
+
+    
+    #client_thread = threading.Thread(target=cliente)
+    #client_thread.start()
+
