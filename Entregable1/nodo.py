1adaeccca70045513c6864c6df4ad9bb1fb9d648
Mauricio Perez <80078495+ppmau@users.noreply.github.com>
2024-10-12 14:29:36 -0600

    Debugg

--- Entregable1/nodo.py
+++ Entregable1/nodo.py
@@ -21,7 +21,7 @@ def asignar_puerto():
 def server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        server_socket.bind(('0.0.0.0', PORT))
+        server_socket.bind(('0.0.0.0',PORT))
         server_socket.listen(5)
         while True:
             client_socket, client_address = server_socket.accept()
