e2f7605082e3c73d8bfc2b3f4a5b322f9c38fe1c
Mauricio Perez <80078495+ppmau@users.noreply.github.com>
2024-11-12 11:51:24 -0600

    Agregando carpeta para entregable 2.Gestion de sala de emeregencia

---
+++ hospitalSD/gestion_doctores.py
@@ -0,0 +1,97 @@
+import os
+
+def mostrarOpDoctores():
+    opcionMenu = 0
+    try:
+        while opcionMenu != 3:
+            os.system('cls') 
+            print("         Gestión de Doctores         \n")
+            print("1.Lista de doctores disponibles ")
+            print("2.Actualizar datos de un doctor ")
+            print("3.Salir")
+            opcionMenu = int(input("Seleccione una opción: "))
+            if opcionMenu > 3:
+                input("Seleccione una opción válida. Enter para continuar...")
+            if int(opcionMenu) == 1:
+                listarDoctores()
+            if int(opcionMenu) == 2:
+                actualizarDoctores()
+    except:
+        input("Ingrese un numero correcto. Enter para continuarrrrrrrr...")
+        mostrarOpDoctores()
+
+def listarDoctores():
+    os.system('cls')
+    print("Lista de doctores: ")
+    input("Enter para continuar...")
+
+
+def actualizarDoctores():
+    opcionActualizar = int(0)
+    try:
+        while opcionActualizar != 4:
+            os.system('cls')
+            print("         Actualizar Doctores         \n")
+            print("1. Editar un doctor")
+            print("2. Agregar un doctor")
+            print("3. Dar de baja un doctor")
+            print("4. Salir")
+            opcionActualizar = int(input("Seleccione una opción: "))
+            if opcionActualizar >= 5:
+                input("Seleccione una opción válida. Enter para continuar...")
+            if int(opcionActualizar) == 1:
+                mostrarOpEditarDoctor()
+            if int(opcionActualizar) == 2:
+                mostrarOpAgregarDoctor()
+            if int(opcionActualizar) == 3:
+                mostrarOpBajaDoctor()
+    except:
+        input("Ingrese un numero correcto. Enter para continuar...")
+        actualizarDoctores()
+
+def mostrarOpEditarDoctor():
+    print("     Edición de datos de doctor")
+    listarDoctores()
+    try:
+        idDoctor = int(input("Ingrese el id del doctor a editar: "))
+        print("Validación de que id es existente") ######Trabajo en BASES
+        existeId = True
+        if existeId == True:
+            print("1.Modificar nombre")
+            print("2.Modificar curp")
+            print("3.Modificar nombre y curp")
+            datoActualizar = input("Ingrese la opcion deseada: ")
+            editarDoctorBD(idDoctor, datoActualizar)
+    except:
+        input("Ingrese un número válido. Enter para continuar...")
+        mostrarOpEditarDoctor()
+
+def editarDoctorBD(idDoctor, campo):
+    print("Update en base del doctor. Exitoso") ##Trabajo en BASES 
+
+def mostrarOpAgregarDoctor():
+    nombreDoctor = input("Nombre doctor: ")
+    curpDoctror = input("CURP: ")
+    insertarDoctorBD(nombreDoctor,curpDoctror)
+
+def insertarDoctorBD(nombreDoctor,curpDoctor):
+    input("Insertando doctor. Enter para continuar...") ##Trabajo en BASES
+
+def mostrarOpBajaDoctor():
+    print("     Baja de datos Doctor")
+    listarDoctores()
+    try:
+        idDoctor = int(input("Ingrese el id del doctor a dar de baja: "))
+        print("Validación de que id es existente") ######Trabajo en BASES
+        existeId = True
+        if existeId == True:
+            bajaDoctorBD(idDoctor)
+        else:
+            input("Numero de id invalido. Enter para continuar...")
+            os.system('cls')
+    except:
+        input("Ingrese un número válido. Enter para continuar...")
+        mostrarOpBajaDoctor()
+
+def bajaDoctorBD(idDoctor):
+    input("Dando de baja Doctor. Enter para continuar...") ##Trabajo en BASES
---
+++ hospitalSD/sala_emergencia.py
@@ -0,0 +1,79 @@
+import os
+import gestion_doctores
+
+
+def mostrarOpciones():
+    opcionMenu = 0
+    try:
+        while opcionMenu != 3:
+            os.system('cls') 
+            if opcionMenu > 3:
+                input("Seleccione una opción válida. Enter para continuar...")
+                os.system('cls')  
+            print("         Sala de emergencias 1          \n")
+            print("1.Registro visita")
+            print("2.Gestionar salas de emergencia")
+            print("3.Salir")
+            opcionMenu = int(input("Seleccione una opción: "))
+            if int(opcionMenu) == 1:
+                mostrarOpRegistro()
+            if int(opcionMenu) == 2:
+                mostrarOpGestion()
+    except:
+        input("Ingrese un numero correcto. Enter para continuar...")
+        mostrarOpciones()
+
+def mostrarOpRegistro():
+    opcionMenu = 0
+    try:
+        nombrePaciente = input("Nombre del paciente: ")
+        edadPaciente = int(input("Edad paciente: "))
+        descripcionEmergencia = input("Descripción de la emergencia: ")
+        insertarEmergencia(nombrePaciente,edadPaciente,descripcionEmergencia)
+        mensaje = "Registro finalizado."
+    except ValueError:
+        mensaje = "Datos erroneos, repita el registro y digite la edad correctamente."
+    finally:
+        input(f"{mensaje} Enter para continuar...")
+    os.system('cls')
+
+def mostrarOpGestion():
+    opcionMenu = 0
+    try:
+        while opcionMenu != 4:
+            os.system('cls') 
+            if opcionMenu > 3:
+                input("Seleccione una opción válida. Enter para continuar...")
+                os.system('cls')  
+            print("         Gestion de sala de emergencias 1         \n")
+            print("1.Doctores")
+            print("2.Pacientes")
+            print("3.Trabajadores sociales")
+            print("4.Salir")
+            opcionMenu = int(input("Seleccione una opción: "))
+            if int(opcionMenu) == 1:
+                gestion_doctores.mostrarOpDoctores()
+            if int(opcionMenu) == 2:
+                mostrarOpPacientes()
+            if int(opcionMenu) == 3:
+                mostrarOpTrabajadores()
+    except:
+        input("Ingrese un numero correcto. Enter para continuar...")
+        mostrarOpGestion()
+
+def mostrarOpPacientes():
+    os.system('cls') 
+    print("Pacientes")
+def mostrarOpTrabajadores():
+    os.system('cls') 
+    print("Trabajadores")
+
+def insertarEmergencia(nombrePaciente,edadPaciente,descripcionEmergencia):
+    print("Insertando emergencia")
+
+def main():
+    mostrarOpciones()
+
+
+
+main() 
