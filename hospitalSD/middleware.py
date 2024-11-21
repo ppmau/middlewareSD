24135ef78766c7e08280abc3900203a5a5d12af1
Mauricio Perez <80078495+ppmau@users.noreply.github.com>
2024-11-21 12:37:04 -0600

    Corrigiendo bugs para leer nodo

--- hospitalSD/middleware.py
+++ hospitalSD/middleware.py
@@ -23,6 +23,8 @@ def server(salaEmergencia):
     PORT, ipNode= asignar_info_nodo()
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     nodoMaestro = asigna_nodo_maestro(ipNode)
+    print(nodoMaestro)
+    print(salaEmergencia)
     if salaEmergencia == str(nodoMaestro[0]):
         print("Estas en el nodo maestro")
     else:
