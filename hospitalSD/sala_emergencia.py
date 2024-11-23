819ecca14e6ce7d6225b0ac930f774b264cf7897
Mauricio Perez <80078495+ppmau@users.noreply.github.com>
2024-11-23 15:59:58 -0600

    Corrigiendo bugs para leer nodo

--- hospitalSD/sala_emergencia.py
+++ hospitalSD/sala_emergencia.py
@@ -120,7 +120,125 @@ def mostrarOpCerrarVisita():
     except Exception as e:
         input("Digite un numero valido. Enter para continuar...")
         mostrarOpCerrarVisita()
-        
+
+def server(server_ready):
+    PORT, ipNode= asignar_info_nodo()
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+    nodoMaestro = asigna_nodo_maestro(ipNode)
+    print(f"Servidor escuchando {ipNode}")
+    try:
+        server_socket.bind((ipNode,PORT))
+        server_socket.listen(5)
+        while True:
+            server_ready.set()
+            client_socket, client_address = server_socket.accept()
+            data = client_socket.recv(1024).decode()
+            client_socket.send(f"El nodo {ipNode} ha recibido el mensaje: {data}".encode() )
+            print(f"\nMensaje: {data} recibido desde {client_address[0]}".encode())
+            if ipNode == nodoMaestro[1]: #Instruccion recibida al nodo maestro
+                print("Estas en el nodo maestro")
+                replicarInformacion(data)
+                distribuirInformacion(data,nodoMaestro)
+            else:
+                print("No estas en el nodo maestro")
+                #enviarInformacion(data,ipNode)
+
+    except Exception as e:
+        print(f"Error en el servidor: {e}")
+    finally:
+        server_socket.close()
+
+def cliente(mensaje,puerto,ipDestino,serverReady):
+    try:
+        serverReady.wait()
+        print("Mandando mensaje")
+        print(mensaje)
+        print(puerto)
+        print(ipDestino)
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+        client_socket.connect((ipDestino, puerto))
+        client_socket.send(mensaje.encode())
+        response = client_socket.recv(1024).decode()
+        print(f"{response}")
+        input("\nEnter para continuar... ")
+    except Exception as e:
+        print(f"No se pudo conectar con el nodo en {ipDestino}:{puerto}. Conexión rechazada.{e}")
+        input("Presione enter para continuar...")
+    finally:
+        client_socket.close()
+
+def replicarInformacion(data):
+    print(f"Estas en replicar informacion {data}")
+    instruccion, tabla, datos = data.split("|")
+    if instruccion == "INSERT":
+        comunicacion_base.insertar_en_tabla(datos.split(','),tabla)
+    if instruccion == "UPDATE":
+        datos = datos.split(',')
+        id = datos[0]
+        campo = datos[1]
+        valor = datos[2]
+        print(f"ID: {id} CAMPO: {campo} tabla {tabla} valor: {valor}")
+        comunicacion_base.actualizar_tabla(id,campo,tabla,valor)
+    if instruccion == "DELETE":
+        comunicacion_base.eliminar_en_tabla(datos,tabla)
+
+def distribuirInformacion(data,nodoMaestro):
+    with open("prioridadNodos.txt", "r") as listaNodos:
+        for nodo in listaNodos:
+            infoNodo = nodo.strip().split(',')
+            if infoNodo[1] != nodoMaestro[1]:
+                print("Recorriendo nodos")
+                cliente(data,int(infoNodo[2]),infoNodo[1])
+
+def asignar_info_nodo():
+    osComand = "ip -4 addr show ens33 | awk '/inet / {print $2}' | cut -d/ -f1"
+    ipNode = subprocess.check_output(osComand, shell=True, text=True).strip()
+    
+    with open("prioridadNodos.txt", "r") as nodeRelation:
+        for ports in nodeRelation:
+            portNode = ports.strip().split(',')
+            if portNode[1] == ipNode:
+                PORT = int(portNode[2])
+    print(f"puerto asignado PORT:{PORT}")
+
+def asigna_nodo_maestro(ipNodoActual):
+    with open("prioridadNodos.txt", "r") as nodeRelation:
+        for ports in nodeRelation:
+            portNode = ports.strip().split(',')
+            print(portNode[1], ipNodoActual)
+            if verificar_conexion(int(portNode[2]),portNode[1]):
+                print("Maestro")
+                return [portNode[0], portNode[1], portNode[2]]
+            else:
+                if portNode[1] == ipNodoActual:
+                    return [portNode[0], portNode[1], portNode[2]]
+                
+def verificar_conexion(puerto, ipDestino):
+    try:
+        # Crear socket para el cliente
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.settimeout(5)  # Tiempo de espera para evitar bloqueos
+
+        # Intentar conectar al nodo
+        client_socket.connect((ipDestino, puerto))
+        return True
+    except (ConnectionRefusedError, OSError , socket.timeout) as e:
+        # Conexión fallida
+        return False
+    finally:
+        # Asegurarse de cerrar el socket
+        client_socket.close()
+
+def inicializarMiddleware():
+    # Crear el hilo para el servidor
+    server_ready = threading.Event()
+    server_thread = threading.Thread(target=server, args=(server_ready,))
+    server_thread.start()
+    client_thread = threading.Thread(target=cliente, args=('hola',12345,'192.168.252.134',server_ready))
+    client_thread.start()
+    #sala_emergencia.mostrarOpciones(server_ready)
 # def main():
 #     global server_ready
 #     salaEmergencia = 1
