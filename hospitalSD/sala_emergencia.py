f986eedc08de652506d16f858ba42f54e9bf9830
Mauricio Perez <80078495+ppmau@users.noreply.github.com>
2024-11-23 14:29:11 -0600

    Corrigiendo bugs para leer nodo

--- hospitalSD/sala_emergencia.py
+++ hospitalSD/sala_emergencia.py
@@ -50,8 +50,9 @@ def mostrarOpRegistro(servidor_list):
             edadPaciente = int(input("Edad paciente: "))
             descripcionEmergencia = input("Descripción de la emergencia: ")
             puertoNodo, ipNodo= middleware.asignar_info_nodo()
-            ipMaestro, puertoMaestro = middleware.asigna_nodo_maestro(ipNodo)  
-            mensaje = "INSERT|tbl_doctores|Jose Mauricio,PEPM960630HDF"
+            ipMaestro, puertoMaestro = middleware.asigna_nodo_maestro(ipNodo)
+            #mensaje = "INSERT|tbl_doctores|Jose Mauricio,PEPM960630HDF"
+            mensaje = nombrePaciente + '|' + edadPaciente + '|' + descripcionEmergencia
             middleware.cliente(mensaje,12345,'192.168.252.134',servidor_list)
             #client_thread = threading.Thread(target=middleware.cliente, args=(mensaje,12345,'192.168.252.134',servidor_list))
             #client_thread.start() #Envia informacion directamente al server en nodo maestro 
