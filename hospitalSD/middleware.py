7ae525e3ab76385b5a73a696e23da73a41c9e4af
Mauricio Perez <80078495+ppmau@users.noreply.github.com>
2024-11-23 19:45:03 -0600

    Solucionando bugs despues de limpieza de codigo

--- hospitalSD/middleware.py
+++ hospitalSD/middleware.py
@@ -75,7 +75,6 @@ def enviaInstruccion(mensaje,puerto,ipDestino): #Función para crear el hilo que
     print(f"mandando mensaje en envia instruccion {mensaje}")
     client_thread = threading.Thread(target=cliente, args=(mensaje,puerto,ipDestino))
     client_thread.start()
-    input()
 
 def mandarMensajeNodo(mensaje):
     print(mensaje)
@@ -137,7 +136,4 @@ def asigna_nodo_maestro(ipNodoActual):
 
 #inicializarMiddleware('1')
 #mandarMensajeNodo("INSERT|tbl_doctores|Jose Mauricio, PEPM960630HDF|")
-
-inicializarMiddleware()
-
 
