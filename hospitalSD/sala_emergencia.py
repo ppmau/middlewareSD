85a18429fd94ecc4ca9ef8b71daf94aa414f418c
Mauricio Perez <80078495+ppmau@users.noreply.github.com>
2024-11-23 18:06:31 -0600

    Implementando flujo para replicacion

--- hospitalSD/sala_emergencia.py
+++ hospitalSD/sala_emergencia.py
@@ -127,6 +127,7 @@ def server():
             if data:
                 client_socket.send(f"El nodo {ipNode} ha recibido el mensaje: {data}".encode() )
                 print(f"\nMensaje: {data} recibido desde {client_address[0]}".encode())
+                print (ipNode + nodoMaestro[1])
                 if ipNode == nodoMaestro[1]: #Instruccion recibida al nodo maestro
                     print("Estas en el nodo maestro")
                     replicarInformacion(data)
