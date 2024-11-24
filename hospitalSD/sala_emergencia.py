6ff393ee2d997d840129c07cca87686bf7654f23
Mauricio Perez <80078495+ppmau@users.noreply.github.com>
2024-11-23 18:01:54 -0600

    Corrigiendo bugs para leer nodo

--- hospitalSD/sala_emergencia.py
+++ hospitalSD/sala_emergencia.py
@@ -49,10 +49,10 @@ def mostrarOpRegistro():
             nombrePaciente = input("Nombre del paciente: ")
             edadPaciente = int(input("Edad paciente: "))
             descripcionEmergencia = input("Descripción de la emergencia: ")
-            mensaje = nombrePaciente + '|' + str(edadPaciente) + '|' + descripcionEmergencia
+            mensaje = 'INSERT' + '|' + 'tbl_pacientes' + '|' + nombrePaciente + str(edadPaciente) + ',' + descripcionEmergencia
             puertoNodo, ipNodo= asignar_info_nodo()
             ipMaestro, puertoMaestro = asigna_nodo_maestro(ipNodo)
-            client_thread = threading.Thread(target=cliente, args=(mensaje,12345,'192.168.252.134'))
+            client_thread = threading.Thread(target=cliente, args=(mensaje,12345,ipMaestro))
             client_thread.start() #Envia informacion directamente al server en nodo maestro 
 
             #gestion_pacientes.insertaPacienteBD(nombrePaciente,edadPaciente,descripcionEmergencia)
