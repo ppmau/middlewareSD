8f95d27c0b910d8199ecb18eac2c7d70e3ee5af8
Mauricio Perez <80078495+ppmau@users.noreply.github.com>
2024-11-26 02:14:24 -0600

    Cambiando listado de pacientes

--- hospitalSD/comunicacion_base.py
+++ hospitalSD/comunicacion_base.py
@@ -31,12 +31,13 @@ def lista_tabla(tabla):
                             p.v_nombre AS nombre_paciente, 
                             p.v_edad, 
                             p.v_emergencia, 
-                            v.i_id_sala, 
-                            v.i_id_cama, 
+                            v.i_id_sala AS sala_emergencia, 
+                            v.i_id_cama AS cama, 
                             d.v_nombre AS nombre_doctor
                         FROM tbl_pacientes p
                         INNER JOIN tbl_visitas v ON p.i_id_paciente = v.i_id_paciente
-                        INNER JOIN tbl_doctores d ON v.i_id_doctor = d.i_id_doctor;"""
+                        INNER JOIN tbl_doctores d ON v.i_id_doctor = d.i_id_doctor
+                        """
             cursor.execute(consulta)
             resultados = cursor.fetchall()
             for fila in resultados:
