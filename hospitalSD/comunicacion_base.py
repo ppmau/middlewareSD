26f51010d99a692bf872d4a3d34cf1862360f247
Mauricio Perez <80078495+ppmau@users.noreply.github.com>
2024-11-26 02:09:00 -0600

    Cambiando listado de pacientes

--- hospitalSD/comunicacion_base.py
+++ hospitalSD/comunicacion_base.py
@@ -26,7 +26,9 @@ def lista_tabla(tabla):
                 id, nombre, curp= fila
                 print(f"ID: {id}, Nombre: {nombre}, CURP: {curp}")
         if tabla == "tbl_pacientes":
-            consulta = f"SELECT i_id_paciente, v_nombre, v_edad, v_emergencia FROM {tabla}"
+            consulta = f""""SELECT i_id_paciente, p.v_nombre, p.v_edad, p.v_emergencia, v.i_id_sala, v.i_id_cama, d.v_nombre FROM {tabla} p
+            INNER JOIN tbl_visitas v ON p.i_id_paciente = v.i_id_paciente
+            INNER JOIN tbl_doctores d ON v.i_id_doctor = d.i_id_doctor"""
             cursor.execute(consulta)
             resultados = cursor.fetchall()
             for fila in resultados:
